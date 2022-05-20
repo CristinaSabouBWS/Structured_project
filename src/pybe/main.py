307399f52314773d3963a18192c407c7be4c08fc
Cristina Sabou <cristina.sabou@clever-software-solutions.com>
2022-05-20 18:54:51 +0300

    Updates

--- src/pybe/main.py
+++ src/pybe/main.py
@@ -4,9 +4,10 @@ from pybe.prime_numbers.prime_to_file import write_prime_numbers
 from pybe.fibonacci.fibo_number import print_fibo
 from pybe.divisors.divisors import print_divisors
 from pybe.combine_list.combine_lists import combined_sorted_list
+from pybe.text_analysis.text_analysis import read_file
 
 #Binary search
-x = get_index_from_file()
+x = get_index_from_file("/Users/bws/work/newproject/src/pybe/binary_search/input.txt")
 
 #Prime numbers < N
 primeList = print_list()
@@ -19,6 +20,9 @@ print_divisors()
 
 #Given a file with 2 sorted lists of integers (each list on its row) 
 # Print a single list with all of the elements from the input lists, such that the single list is also sorted.
-combined_sorted_list()
+combined_sorted_list("/Users/bws/work/newproject/src/pybe/combine_list/input_sorted_list.txt")
 
-#write_prime_numbers()
+#Print all the prime numbers up to it in a file, one on each line
+#write_prime_numbers("/Users/bws/work/newproject/src/pybe/prime_numbers/output.txt")
+
+read_file("/Users/bws/work/newproject/src/pybe/text_analysis/text_article.txt")
